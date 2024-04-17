a8c557ad62b2681f2fb630a4e8df6322cb216ab8
JulianJekov <drmsz@abv.bg>
2024-04-17 22:05:30 +0300

    Adding home page and home controller

---
+++ src/main/java/org/example/springboottodoapplication/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package org.example.springboottodoapplication.controllers;
+
+import org.example.springboottodoapplication.service.TodoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    private final TodoItemService todoItemService;
+
+    @Autowired
+    public HomeController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
+    @GetMapping("/")
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", this.todoItemService.getAll());
+        return modelAndView;
+    }
+}
